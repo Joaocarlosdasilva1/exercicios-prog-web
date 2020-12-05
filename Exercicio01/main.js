3c37a8cf390ce16000122480a3d04691e6720383
martoxak1 <47401364+martoxak1@users.noreply.github.com>
2020-12-05 13:32:29 -0300

    Add files via upload

---
+++ Exercicio01/main.js
@@ -0,0 +1,11 @@
+var nome = "Joao";
+var idade = 28;
+
+console.log("Nome = " + nome + " | " + typeof(nome));
+console.log("Idade = " + idade +" | " + typeof(idade));
+
+if (idade >= 18) {
+	console.log('Maior de Idade');
+} else{
+	console.log('Menor de Idade');
+}
